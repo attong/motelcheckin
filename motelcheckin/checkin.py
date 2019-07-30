9aa572cb45b1003b92cd83935736211ea2ac22bf
Anthony Tong <atong30@gatech.edu>
2019-07-30 16:47:17 -0700

    added find customers view

--- motelcheckin/checkin.py
+++ motelcheckin/checkin.py
@@ -5,7 +5,17 @@ from flask import (
 
 bp = Blueprint("checkin", __name__)
 
-    
+@bp.route("/findcustomer", methods=("GET", "POST"))
+def check_in(id):
+    if request.method == "GET":
+        return render_template("find_customer.html")
+    elif request.method == "POST":
+    	# if find, redirect to checkin page
+    	#else add customer
+    	return "finding customer!"
+
+@bp.route("/addcustomer", methods=("GET","POST"))
+
 @bp.route("/checkin/<id>", methods=("GET", "POST"))
 def check_in(id):
     if request.method == "GET":
